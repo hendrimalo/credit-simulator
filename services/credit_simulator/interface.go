cb84edc50bce144412b1b53b2c3d98aeb1b93d87
hendrimalo <hendri.malo@yahoo.co.id>
2023-06-02 02:33:29 +0700

    interface creditSimulator readfile

--- services/credit_simulator/interface.go
+++ services/credit_simulator/interface.go
@@ -2,9 +2,12 @@ package service
 
 import (
 	models "golang-credit-simulator/models/credit_simulator"
+
+	"github.com/gin-gonic/gin"
 )
 
 type CreditSimulatorUsecase interface {
+	ReadFile(c *gin.Context) (res models.CreditSimulator, err error)
 	CheckRateAndValidationCredit(req models.CreditSimulator) (rate float32, err error)
 	GenerateCreditSimulation(rate float32, req models.CreditSimulator) (res []models.CreditSimulatorRespon)
 }
